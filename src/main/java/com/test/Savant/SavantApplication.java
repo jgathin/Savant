cc8cd0fc2e783212daf0c7596f965f7210b47c78
Jeremy G <jgathin@gmail.com>
2022-01-16 19:21:32 -0600

    first commit

---
+++ src/main/java/com/test/Savant/SavantApplication.java
@@ -0,0 +1,13 @@
+package com.test.Savant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavantApplication.class, args);
+	}
+
+}
