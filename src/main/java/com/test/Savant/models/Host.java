4eeaa9e522a5ba83261e0eb503f57b5ff9a889d4
Jeremy G <jgathin@gmail.com>
2022-01-26 21:46:40 -0600

    added additional properties to Host

--- src/main/java/com/test/Savant/models/Host.java
+++ src/main/java/com/test/Savant/models/Host.java
@@ -11,8 +11,25 @@ public class Host extends Device {
     private int videoZones;
     private int totalZones;
     private int supportedLights;
+    private int supportedShades;
+    private int loadGroups;
+    private int mobileDevices;
+    private int ipVideoDevices;
+    private int ipAudioDevices;
     private int supportedSensors;
     private boolean embedded;
+    private boolean tiling;
+    private boolean thirdPartySwitching;
+    private boolean energyService;
+    private boolean bacnetKNXIntegration;
+    private int totalEntryStations;
+    private int totalTStats;
+    private int totalGarageDoors;
+    private int totalDoorLocks;
+    private int totalSecurityEndpoints;
+    private int ipCameras;
+    private boolean intercom;
+
     @OneToOne(cascade = CascadeType.ALL)
     private ControllableIO controlIO;
     @OneToOne(cascade = CascadeType.ALL)
@@ -53,6 +70,138 @@ public class Host extends Device {
 
     public void setSignalIO (SignalIO signalIO) {this.signalIO = signalIO;}
     public SignalIO getSignalIO () {return this.signalIO;}
+
+    public int getSupportedShades() {
+        return supportedShades;
+    }
+
+    public void setSupportedShades(int supportedShades) {
+        this.supportedShades = supportedShades;
+    }
+
+    public int getLoadGroups() {
+        return loadGroups;
+    }
+
+    public void setLoadGroups(int loadGroups) {
+        this.loadGroups = loadGroups;
+    }
+
+    public int getMobileDevices() {
+        return mobileDevices;
+    }
+
+    public void setMobileDevices(int mobileDevices) {
+        this.mobileDevices = mobileDevices;
+    }
+
+    public int getIpVideoDevices() {
+        return ipVideoDevices;
+    }
+
+    public void setIpVideoDevices(int ipVideoDevices) {
+        this.ipVideoDevices = ipVideoDevices;
+    }
+
+    public int getIpAudioDevices() {
+        return ipAudioDevices;
+    }
+
+    public void setIpAudioDevices(int ipAudioDevices) {
+        this.ipAudioDevices = ipAudioDevices;
+    }
+
+    public boolean isEmbedded() {
+        return embedded;
+    }
+
+    public boolean isTiling() {
+        return tiling;
+    }
+
+    public void setTiling(boolean tiling) {
+        this.tiling = tiling;
+    }
+
+    public boolean isThirdPartySwitching() {
+        return thirdPartySwitching;
+    }
+
+    public void setThirdPartySwitching(boolean thirdPartySwitching) {
+        this.thirdPartySwitching = thirdPartySwitching;
+    }
+
+    public boolean isEnergyService() {
+        return energyService;
+    }
+
+    public void setEnergyService(boolean energyService) {
+        this.energyService = energyService;
+    }
+
+    public boolean isBacnetKNXIntegration() {
+        return bacnetKNXIntegration;
+    }
+
+    public void setBacnetKNXIntegration(boolean bacnetKNXIntegration) {
+        this.bacnetKNXIntegration = bacnetKNXIntegration;
+    }
+
+    public int getTotalEntryStations() {
+        return totalEntryStations;
+    }
+
+    public void setTotalEntryStations(int totalEntryStations) {
+        this.totalEntryStations = totalEntryStations;
+    }
+
+    public int getTotalTStats() {
+        return totalTStats;
+    }
+
+    public void setTotalTStats(int totalTStats) {
+        this.totalTStats = totalTStats;
+    }
+
+    public int getTotalGarageDoors() {
+        return totalGarageDoors;
+    }
+
+    public void setTotalGarageDoors(int totalGarageDoors) {
+        this.totalGarageDoors = totalGarageDoors;
+    }
+
+    public int getTotalDoorLocks() {
+        return totalDoorLocks;
+    }
+
+    public void setTotalDoorLocks(int totalDoorLocks) {
+        this.totalDoorLocks = totalDoorLocks;
+    }
+
+    public int getTotalSecurityEndpoints() {
+        return totalSecurityEndpoints;
+    }
+
+    public void setTotalSecurityEndpoints(int totalSecurityEndpoints) {
+        this.totalSecurityEndpoints = totalSecurityEndpoints;
+    }
+
+    public int getIpCameras() {
+        return ipCameras;
+    }
+
+    public void setIpCameras(int ipCameras) {
+        this.ipCameras = ipCameras;
+    }
+
+    public boolean isIntercom() {
+        return intercom;
+    }
+
+    public void setIntercom(boolean intercom) {
+        this.intercom = intercom;
+    }
 
     public boolean hostSelection (int v, int t) {
         if (v < this.videoZones && t < this.totalZones ) {
